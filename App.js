84eadf439c713b8a5a21c6eba5efb185ce1c39b9
Gayatri <117992250+gayatripotnuru@users.noreply.github.com>
2024-12-21 00:12:40 +0530

    updated the files

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
  
  //using react to print hello world
  
- var y= React.createElement("div",null,React.createElement("h2",null,"hello world using react"),
+ var y= React.createElement("div",null,React.createElement("h2",null,"hello world with react"),
         React.createElement("p",null,"This is nested element"));
         var htmlRoot = document.getElementById("root");
         var rootReact = ReactDOM.createRoot(htmlRoot);
